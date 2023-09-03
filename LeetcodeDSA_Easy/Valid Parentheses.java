b92473b9a66ad0c974a2efe5cb8ba6a29025a845
Bhumika Manoj Gajrani <72298412+bhumikagajrani@users.noreply.github.com>
2023-09-04 04:02:31 +0530

    Create Valid Parentheses.java

---
+++ LeetcodeDSA_Easy/Valid Parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{')
+            {
+                stack.push(s.charAt(i));
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                char top=stack.peek();
+                if((top=='('&&s.charAt(i)==')') 
+                || (top=='['&&s.charAt(i)==']') 
+                || (top=='{'&&s.charAt(i)=='}'))
+                {
+                    stack.pop();
+
+                }
+                else
+                return false;
+            }
+        } 
+       return stack.isEmpty();
+    }
+}
