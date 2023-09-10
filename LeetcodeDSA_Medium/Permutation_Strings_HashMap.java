95224ef24128d01794d97aaaa59f13580e466b7f
Bhumika Manoj Gajrani <72298412+bhumikagajrani@users.noreply.github.com>
2023-09-11 04:10:57 +0530

    Update Permutation_Strings_HashMap.java

--- LeetcodeDSA_Medium/Permutation_Strings_HashMap.java
+++ LeetcodeDSA_Medium/Permutation_Strings_HashMap.java
@@ -1,41 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
+        if(s1.length()>s2.length()) return false;
         HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2=new HashMap<>();
         for(int i=0;i<s1.length();i++)
         {
             
             map1.put(s1.charAt(i),map1.getOrDefault(s1.charAt(i),0)+1);
+            map2.put(s2.charAt(i),map2.getOrDefault(s2.charAt(i),0)+1);
         }
-        for(int i=0;i<=s2.length()-s1.length();i++)
+        for(int i=0;i<s2.length()-s1.length();i++)
         {
-            String sub = s2.substring(i,s1.length()+i);
-            HashMap<Character,Integer> map2=new HashMap<>();
-            for(int j=0;j<sub.length();j++)
-            {
-                
-                    map2.put(sub.charAt(j),map2.getOrDefault(sub.charAt(j),0)+1);
-               
-            }
-            if(match(map1,map2))
-            {
-                return true;
-            }
+           if(match(map1,map2))
+           {
+               return true;
+           }
 
+           map2.put(s2.charAt(i),map2.get(s2.charAt(i))-1);
+           if(map2.get(s2.charAt(i))==0)
+           {
+                map2.remove(s2.charAt(i));
+           }
+
+           map2.put(s2.charAt(i+s1.length()),map2.getOrDefault(s2.charAt(i+s1.length()),0)+1);
+           
+           
         }
-
-        return false;
+        return match(map1,map2);
+        
     }
 
     private boolean match(HashMap<Character,Integer> map1, HashMap<Character,Integer> map2)
     {
-        for(char key:map1.keySet())
-        {
-            if(map1.get(key)-map2.getOrDefault(key,-1)!=0)
-            {
-                return false;
-            }
-        }
-        return true;
+        return map1.equals(map2);
     }
 }
