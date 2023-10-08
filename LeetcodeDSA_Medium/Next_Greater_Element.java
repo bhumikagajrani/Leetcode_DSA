024ff0ecf61a830ec2fbf04a240d87e961df28d5
Bhumika Manoj Gajrani <72298412+bhumikagajrani@users.noreply.github.com>
2023-10-08 19:01:29 +0530

    Create Next_Greater_Element.java

---
+++ LeetcodeDSA_Medium/Next_Greater_Element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int ans[] = new int[nums.length];
+        Stack<Integer> st = new Stack<>();
+        for(int i=2*nums.length-1;i>=0;i--)
+        {
+            while(!st.isEmpty() && st.peek()<=nums[i%nums.length])
+            {
+                st.pop();
+            }
+            if(i<nums.length)
+            {
+                    if(st.isEmpty())
+                {
+                    ans[i]=-1;
+                }
+                else
+                {
+                    ans[i]=st.peek();
+                }
+            }
+            
+            st.push(nums[i%nums.length]);
+        }
+        return ans;
+    }
+}
