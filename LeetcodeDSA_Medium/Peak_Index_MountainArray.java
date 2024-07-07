2a59e6cd1459241944c9ff8d2512ee681d154e86
Bhumika Gajrani <72298412+bhumikagajrani@users.noreply.github.com>
2024-07-07 13:15:53 +0530

    Create Peak_Index_MountainArray.java

---
+++ LeetcodeDSA_Medium/Peak_Index_MountainArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        
+        int n=arr.length;
+        int low=0, high=n-1;
+        while(low<high) {
+            int mid = (low+high)/2;
+
+            if(arr[mid]<arr[mid+1]) {
+                low=mid+1;
+            }
+            else {
+                high=mid;
+            }
+        }
+        return low;
+    }
+}
