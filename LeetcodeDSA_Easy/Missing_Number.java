3163fc26138347225812be5762f6cb9964d796d8
Bhumika Gajrani <72298412+bhumikagajrani@users.noreply.github.com>
2024-09-22 18:29:44 +0530

    Create Missing_Number.java

---
+++ LeetcodeDSA_Easy/Missing_Number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+    
+      int n = nums.length;
+      int sum=0;
+      int expsum = (n*(n+1))/2;
+      
+      for(int i=0;i<n;i++) {
+        sum+=nums[i];
+      }
+
+      return (expsum-sum);
+        
+        
+    }
+}
